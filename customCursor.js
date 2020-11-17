25c51c9d018c84d931fbb76cfbf7dcbfcd865559
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2020-11-17 14:09:31 +0100

    cursor fixed onscrolling, lacks animation onclick

--- customCursor.js
+++ customCursor.js
@@ -16,8 +16,8 @@ function myCursor(e){
     //console.log(e);
     //of all the properties on console.log, we need pageX and pageY. They tell the location of the mouse on the screen. 
 
-    customCursor.style.top = e.pageY + "px";
-    customCursor.style.left = e.pageX + 'px'; 
+    top: customCursor.style.top = e.clientY + "px";
+    customCursor.style.left = e.clientX + 'px'; 
     //at this point, we have the circle moving with the cursor, but it is uncentered/on top;  transform: translate(-50%, -50%) will fix it. 
     //also, we cannot select anything because the div remains on top of everything else on the page, pointer-events: none; will fix it. 
 }
@@ -28,3 +28,7 @@ navLinks.forEach(link => {
         customCursor.classList.add('link-grow')
     })
 })
+
+
+
+//scrolling issues fixing: 
