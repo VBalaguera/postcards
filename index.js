686c54c8938d22734fe559e314fdab2a97609f09
VBalaguera <61208719+VBalaguera@users.noreply.github.com>
2020-11-17 17:12:41 +0100

    first dissolve on fade out test, not satisfactory

--- index.js
+++ index.js
@@ -27,3 +27,25 @@ function updatesFirst() {
 
 
 
+//fade out transition on scroll for all postcards
+
+$(window).on("load",function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".postcard").each(function() {
+        /* Check the location of each desired element */
+        
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+        } else { //object goes out of view (scrolling up)
+          if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  });
+
+
+  
